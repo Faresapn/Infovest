40dcad985cfa7ce1cef06b32d0bb9c800fc391f4
Qoolqas <qoolqas@gmail.com>
2019-10-24 07:25:52 +0700

    rahman

---
+++ assets/js/js.js
@@ -0,0 +1,17 @@
+$(".image-checkbox").each(function () {
+    if ($(this).find('input[type="checkbox"]').first().attr("checked")) {
+      $(this).addClass('image-checkbox-checked');
+    }
+    else {
+      $(this).removeClass('image-checkbox-checked');
+    }
+  });
+  
+  // sync the state to the input
+  $(".image-checkbox").on("click", function (e) {
+    $(this).toggleClass('image-checkbox-checked');
+    var $checkbox = $(this).find('input[type="checkbox"]');
+    $checkbox.prop("checked",!$checkbox.prop("checked"))
+  
+    e.preventDefault();
+  });
